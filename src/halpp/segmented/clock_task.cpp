ca8c134d4773dae3257dccf5fd314166d68b8d51
Trent Apted <tapted@gmail.com>
2026-06-28 14:18:46 +1000

    Bah

--- src/halpp/segmented/clock_task.cpp
+++ src/halpp/segmented/clock_task.cpp
@@ -10,9 +10,9 @@ void ClockTask::on_time_synced() {
   if (task_.running()) {
     task_.notify();
   } else {
-    // Empricially, we need 108 bytes right now. 512 is plenty. Just don't log in the task thread.
-    // Any call to logging needs ~1.5kB and will instantly crash the task.
-    constexpr TaskConfig config = {.name = "clock_task", .stack_size = 512, .priority = 3};
+    // Empricially, we need 108 bytes right now. 512 is plenty if there's no logging.
+    // Problem: interrupts can randomly take over the task and cause a stack overflow. Humph.
+    constexpr TaskConfig config = {.name = "clock_task", .stack_size = 2048, .priority = 3};
     task_.start(config, nullptr, clock_update_step);
   }
 }
