b8a3746056db0e95972af180012a5758475199fa
Trent Apted <tapted@gmail.com>
2026-07-04 09:29:07 +1000

    Pass a vendor config

--- src/halpp/display/ssd1306.cpp
+++ src/halpp/display/ssd1306.cpp
@@ -2,7 +2,7 @@
 
 #include <esp_lcd_panel_io.h>
 #include <esp_lcd_panel_ops.h>
-#include <esp_lcd_panel_vendor.h>
+#include <esp_lcd_panel_ssd1306.h>
 #include <esp_log.h>
 
 #include "halpp/display/boot_logo.hpp"
@@ -56,14 +56,26 @@ EspResult<void> Ssd1306::init_default_i2c(uint8_t i2c_address, uint16_t width, u
 EspResult<void> Ssd1306::begin() {
   if (!config_.io_handle) return ESP_ERR_INVALID_STATE;
   if (is_initialized()) return ESP_OK;
+  esp_lcd_panel_ssd1306_config_t ssd1306_config = {.height = 64};
+  switch (config_.height) {
+    case 32:
+      ssd1306_config.height = 32;
+      break;
+    case 64:
+      ssd1306_config.height = 64;  // Default, but explicit for clarity.
+      break;
+    default:
+      ESP_LOGE(TAG, "Unsupported SSD1306 height: %d. Only 32 or 64 are supported.", config_.height);
+      return ESP_ERR_INVALID_ARG;
+  }
 
   esp_lcd_panel_dev_config_t panel_config = {
       .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,  // Ignored by monochrome SSD1306
       .data_endian = LCD_RGB_DATA_ENDIAN_BIG,      // Ignored by monochrome SSD1306
       .bits_per_pixel = 1,
       // I2C modules typically don't have a hardware reset pin wired
-      .reset_gpio_num = static_cast<gpio_num_t>(-1),
-      .vendor_config = nullptr,
+      .reset_gpio_num = GPIO_NUM_NC,
+      .vendor_config = &ssd1306_config,
       .flags =
           {
               .reset_active_high = 0,
@@ -74,7 +86,9 @@ EspResult<void> Ssd1306::begin() {
     return err.log(TAG, "Failed to create SSD1306 panel handle");
   }
 
+  // Reset will likely be a no-op because the reset GPIO is not connected.
   if (EspError err = esp_lcd_panel_reset(panel_handle_)) return err;
+
   if (EspError err = esp_lcd_panel_init(panel_handle_)) return err;
 
   // Ensure RAM doesn't show static on boot
@@ -84,7 +98,7 @@ EspResult<void> Ssd1306::begin() {
     clear();
   }
 
-  if (EspError err = esp_lcd_panel_disp_on_off(panel_handle_, true)) return err;
+  if (EspError err = esp_lcd_panel_disp_on_off(panel_handle_, true)) return err;  // 100ms delay.
 
   ESP_LOGI(TAG, "SSD1306 Initialized (%dx%d)", config_.width, config_.height);
   return ESP_OK;
